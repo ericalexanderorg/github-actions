294844b8888eaeb20085dc17d42e078b1d376614
Eric Alexander <eric.alexander@thetradedesk.com>
2019-10-28 22:21:49 -0600

    fixing auto-summry issue

--- auto-summry/entrypoint.sh
+++ auto-summry/entrypoint.sh
@@ -5,7 +5,7 @@ GITHUB_ISSUE_BODY=$(jq --raw-output .issue.body "$GITHUB_EVENT_PATH")
 URL=$(echo "$GITHUB_ISSUE_BODY" | grep -o '(http|https)://[^/"]+')
 
 # Get the summary
-SUMMRY=$(curl -s "http://api.smmry.com/&SM_API_KEY=$SUMMRY_API_KEY&SM_URL=$URL" | jq .sm_api_content)
+SUMMRY=$(curl -s "http://api.smmry.com/&SM_API_KEY=${SUMMRY_API_KEY}&SM_URL=${URL}" | jq .sm_api_content)
 
 # Add a comment to the issue with the Summry
 COMMENTS_URI=$(jq -r ".issue.comments_url" "$GITHUB_EVENT_PATH")
